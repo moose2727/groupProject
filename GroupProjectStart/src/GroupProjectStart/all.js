392c286fbd9dc32f47a9d4e2b0202265c4000fed
Cory Couty <syncrose@gmail.com>
2016-04-18 20:57:26 -0700

    Added delete, and a few other car services

--- GroupProjectStart/src/GroupProjectStart/all.js
+++ GroupProjectStart/src/GroupProjectStart/all.js
@@ -39,6 +39,12 @@ var GroupProjectStart;
             controller: GroupProjectStart.Controllers.AboutController,
             controllerAs: 'controller'
         })
+            .state('edit', {
+            url: '/edit/:id',
+            templateUrl: '/ngApp/views/carEdit.html',
+            controller: GroupProjectStart.Controllers.CarsEditController,
+            controllerAs: 'controller'
+        })
             .state('notFound', {
             url: '/notFound',
             templateUrl: '/ngApp/views/notFound.html'
@@ -68,166 +74,6 @@ var GroupProjectStart;
     });
 })(GroupProjectStart || (GroupProjectStart = {}));
 /// <reference path="wwwroot/ngapp/app.ts" />
-var GroupProjectStart;
-(function (GroupProjectStart) {
-    var Services;
-    (function (Services) {
-        var AccountService = (function () {
-            function AccountService($q, $http, $window) {
-                this.$q = $q;
-                this.$http = $http;
-                this.$window = $window;
-                // in case we are redirected from a social provider
-                // we need to check if we are authenticated.
-                this.checkAuthentication();
-            }
-            // Store access token and claims in browser session storage
-            AccountService.prototype.storeUserInfo = function (userInfo) {
-                // store user name
-                this.$window.sessionStorage.setItem('userName', userInfo.userName);
-                // store claims
-                this.$window.sessionStorage.setItem('claims', JSON.stringify(userInfo.claims));
-            };
-            AccountService.prototype.getUserName = function () {
-                return this.$window.sessionStorage.getItem('userName');
-            };
-            AccountService.prototype.getClaim = function (type) {
-                var allClaims = JSON.parse(this.$window.sessionStorage.getItem('claims'));
-                return allClaims ? allClaims[type] : null;
-            };
-            AccountService.prototype.login = function (loginUser) {
-                var _this = this;
-                return this.$q(function (resolve, reject) {
-                    _this.$http.post('/api/account/login', loginUser).then(function (result) {
-                        _this.storeUserInfo(result.data);
-                        resolve();
-                    }).catch(function (result) {
-                        var messages = _this.flattenValidation(result.data);
-                        reject(messages);
-                    });
-                });
-            };
-            AccountService.prototype.register = function (userLogin) {
-                var _this = this;
-                return this.$q(function (resolve, reject) {
-                    _this.$http.post('/api/account/register', userLogin)
-                        .then(function (result) {
-                        _this.storeUserInfo(result.data);
-                        resolve(result);
-                    })
-                        .catch(function (result) {
-                        var messages = _this.flattenValidation(result.data);
-                        reject(messages);
-                    });
-                });
-            };
-            AccountService.prototype.logout = function () {
-                // clear all of session storage (including claims)
-                this.$window.sessionStorage.clear();
-                // logout on the server
-                return this.$http.post('/api/account/logout', null);
-            };
-            AccountService.prototype.isLoggedIn = function () {
-                return this.$window.sessionStorage.getItem('userName');
-            };
-            // associate external login (e.g., Twitter) with local user account
-            AccountService.prototype.registerExternal = function (email) {
-                var _this = this;
-                return this.$q(function (resolve, reject) {
-                    _this.$http.post('/api/account/externalLoginConfirmation', { email: email })
-                        .then(function (result) {
-                        _this.storeUserInfo(result.data);
-                        resolve(result);
-                    })
-                        .catch(function (result) {
-                        // flatten error messages
-                        var messages = _this.flattenValidation(result.data);
-                        reject(messages);
-                    });
-                });
-            };
-            AccountService.prototype.getExternalLogins = function () {
-                var _this = this;
-                return this.$q(function (resolve, reject) {
-                    var url = "api/Account/getExternalLogins?returnUrl=%2FexternalLogin&generateState=true";
-                    _this.$http.get(url).then(function (result) {
-                        resolve(result.data);
-                    }).catch(function (result) {
-                        reject(result);
-                    });
-                });
-            };
-            // checks whether the current user is authenticated on the server and returns user info
-            AccountService.prototype.checkAuthentication = function () {
-                var _this = this;
-                this.$http.get('/api/account/checkAuthentication')
-                    .then(function (result) {
-                    if (result.data) {
-                        _this.storeUserInfo(result.data);
-                    }
-                });
-            };
-            AccountService.prototype.confirmEmail = function (userId, code) {
-                var _this = this;
-                return this.$q(function (resolve, reject) {
-                    var data = {
-                        userId: userId,
-                        code: code
-                    };
-                    _this.$http.post('/api/account/confirmEmail', data).then(function (result) {
-                        resolve(result.data);
-                    }).catch(function (result) {
-                        reject(result);
-                    });
-                });
-            };
-            // extract access token from response
-            AccountService.prototype.parseOAuthResponse = function (token) {
-                var results = {};
-                token.split('&').forEach(function (item) {
-                    var pair = item.split('=');
-                    results[pair[0]] = pair[1];
-                });
-                return results;
-            };
-            AccountService.prototype.flattenValidation = function (modelState) {
-                var messages = [];
-                for (var prop in modelState) {
-                    messages = messages.concat(modelState[prop]);
-                }
-                return messages;
-            };
-            return AccountService;
-        }());
-        Services.AccountService = AccountService;
-        angular.module('GroupProjectStart').service('accountService', AccountService);
-    })(Services = GroupProjectStart.Services || (GroupProjectStart.Services = {}));
-})(GroupProjectStart || (GroupProjectStart = {}));
-var GroupProjectStart;
-(function (GroupProjectStart) {
-    var Services;
-    (function (Services) {
-        var CarService = (function () {
-            function CarService($resource) {
-                this.$resource = $resource;
-                this.carResource = this.$resource("/api/cars/:id");
-            }
-            CarService.prototype.getCars = function () {
-                return this.carResource.query();
-            };
-            return CarService;
-        }());
-        Services.CarService = CarService;
-        angular.module("GroupProjectStart").service('carService', CarService);
-    })(Services = GroupProjectStart.Services || (GroupProjectStart.Services = {}));
-})(GroupProjectStart || (GroupProjectStart = {}));
-var GroupProjectStart;
-(function (GroupProjectStart) {
-    var Services;
-    (function (Services) {
-        console.log("Test");
-    })(Services = GroupProjectStart.Services || (GroupProjectStart.Services = {}));
-})(GroupProjectStart || (GroupProjectStart = {}));
 var GroupProjectStart;
 (function (GroupProjectStart) {
     var Controllers;
@@ -393,6 +239,30 @@ var GroupProjectStart;
 (function (GroupProjectStart) {
     var Controllers;
     (function (Controllers) {
+        var CarsEditController = (function () {
+            function CarsEditController(carService, $stateParams, $state) {
+                this.carService = carService;
+                this.$stateParams = $stateParams;
+                this.$state = $state;
+                // extracts the car id from the url using $stateParams
+                var carid = this.$stateParams['id'];
+                this.carToEdit = this.carService.getCar(carid);
+            }
+            CarsEditController.prototype.editCar = function () {
+                var _this = this;
+                this.carService.saveCar(this.carToEdit).then(function () {
+                    _this.$state.go('cars');
+                });
+            };
+            return CarsEditController;
+        }());
+        Controllers.CarsEditController = CarsEditController;
+    })(Controllers = GroupProjectStart.Controllers || (GroupProjectStart.Controllers = {}));
+})(GroupProjectStart || (GroupProjectStart = {}));
+var GroupProjectStart;
+(function (GroupProjectStart) {
+    var Controllers;
+    (function (Controllers) {
         var HomeController = (function () {
             function HomeController() {
                 this.message = 'Hello from the home page!';
@@ -418,5 +288,199 @@ var GroupProjectStart;
         }());
         Controllers.AboutController = AboutController;
     })(Controllers = GroupProjectStart.Controllers || (GroupProjectStart.Controllers = {}));
+})(GroupProjectStart || (GroupProjectStart = {}));
+var GroupProjectStart;
+(function (GroupProjectStart) {
+    var Controllers;
+    (function (Controllers) {
+        var DeleteCarController = (function () {
+            function DeleteCarController(carService, $stateParams, $state) {
+                this.carService = carService;
+                this.$stateParams = $stateParams;
+                this.$state = $state;
+            }
+            DeleteCarController.prototype.deleteCar = function () {
+                var _this = this;
+                this.carService.deleteCar(this.$stateParams['id']).then(function () {
+                    _this.$state.go('cars');
+                });
+            };
+            DeleteCarController.prototype.cancel = function () {
+                this.$state.go('controller');
+            };
+            return DeleteCarController;
+        }());
+        Controllers.DeleteCarController = DeleteCarController;
+    })(Controllers = GroupProjectStart.Controllers || (GroupProjectStart.Controllers = {}));
+})(GroupProjectStart || (GroupProjectStart = {}));
+var GroupProjectStart;
+(function (GroupProjectStart) {
+    var Services;
+    (function (Services) {
+        var AccountService = (function () {
+            function AccountService($q, $http, $window) {
+                this.$q = $q;
+                this.$http = $http;
+                this.$window = $window;
+                // in case we are redirected from a social provider
+                // we need to check if we are authenticated.
+                this.checkAuthentication();
+            }
+            // Store access token and claims in browser session storage
+            AccountService.prototype.storeUserInfo = function (userInfo) {
+                // store user name
+                this.$window.sessionStorage.setItem('userName', userInfo.userName);
+                // store claims
+                this.$window.sessionStorage.setItem('claims', JSON.stringify(userInfo.claims));
+            };
+            AccountService.prototype.getUserName = function () {
+                return this.$window.sessionStorage.getItem('userName');
+            };
+            AccountService.prototype.getClaim = function (type) {
+                var allClaims = JSON.parse(this.$window.sessionStorage.getItem('claims'));
+                return allClaims ? allClaims[type] : null;
+            };
+            AccountService.prototype.login = function (loginUser) {
+                var _this = this;
+                return this.$q(function (resolve, reject) {
+                    _this.$http.post('/api/account/login', loginUser).then(function (result) {
+                        _this.storeUserInfo(result.data);
+                        resolve();
+                    }).catch(function (result) {
+                        var messages = _this.flattenValidation(result.data);
+                        reject(messages);
+                    });
+                });
+            };
+            AccountService.prototype.register = function (userLogin) {
+                var _this = this;
+                return this.$q(function (resolve, reject) {
+                    _this.$http.post('/api/account/register', userLogin)
+                        .then(function (result) {
+                        _this.storeUserInfo(result.data);
+                        resolve(result);
+                    })
+                        .catch(function (result) {
+                        var messages = _this.flattenValidation(result.data);
+                        reject(messages);
+                    });
+                });
+            };
+            AccountService.prototype.logout = function () {
+                // clear all of session storage (including claims)
+                this.$window.sessionStorage.clear();
+                // logout on the server
+                return this.$http.post('/api/account/logout', null);
+            };
+            AccountService.prototype.isLoggedIn = function () {
+                return this.$window.sessionStorage.getItem('userName');
+            };
+            // associate external login (e.g., Twitter) with local user account
+            AccountService.prototype.registerExternal = function (email) {
+                var _this = this;
+                return this.$q(function (resolve, reject) {
+                    _this.$http.post('/api/account/externalLoginConfirmation', { email: email })
+                        .then(function (result) {
+                        _this.storeUserInfo(result.data);
+                        resolve(result);
+                    })
+                        .catch(function (result) {
+                        // flatten error messages
+                        var messages = _this.flattenValidation(result.data);
+                        reject(messages);
+                    });
+                });
+            };
+            AccountService.prototype.getExternalLogins = function () {
+                var _this = this;
+                return this.$q(function (resolve, reject) {
+                    var url = "api/Account/getExternalLogins?returnUrl=%2FexternalLogin&generateState=true";
+                    _this.$http.get(url).then(function (result) {
+                        resolve(result.data);
+                    }).catch(function (result) {
+                        reject(result);
+                    });
+                });
+            };
+            // checks whether the current user is authenticated on the server and returns user info
+            AccountService.prototype.checkAuthentication = function () {
+                var _this = this;
+                this.$http.get('/api/account/checkAuthentication')
+                    .then(function (result) {
+                    if (result.data) {
+                        _this.storeUserInfo(result.data);
+                    }
+                });
+            };
+            AccountService.prototype.confirmEmail = function (userId, code) {
+                var _this = this;
+                return this.$q(function (resolve, reject) {
+                    var data = {
+                        userId: userId,
+                        code: code
+                    };
+                    _this.$http.post('/api/account/confirmEmail', data).then(function (result) {
+                        resolve(result.data);
+                    }).catch(function (result) {
+                        reject(result);
+                    });
+                });
+            };
+            // extract access token from response
+            AccountService.prototype.parseOAuthResponse = function (token) {
+                var results = {};
+                token.split('&').forEach(function (item) {
+                    var pair = item.split('=');
+                    results[pair[0]] = pair[1];
+                });
+                return results;
+            };
+            AccountService.prototype.flattenValidation = function (modelState) {
+                var messages = [];
+                for (var prop in modelState) {
+                    messages = messages.concat(modelState[prop]);
+                }
+                return messages;
+            };
+            return AccountService;
+        }());
+        Services.AccountService = AccountService;
+        angular.module('GroupProjectStart').service('accountService', AccountService);
+    })(Services = GroupProjectStart.Services || (GroupProjectStart.Services = {}));
+})(GroupProjectStart || (GroupProjectStart = {}));
+var GroupProjectStart;
+(function (GroupProjectStart) {
+    var Services;
+    (function (Services) {
+        var CarService = (function () {
+            function CarService($resource) {
+                this.$resource = $resource;
+                this.carResource = this.$resource("/api/cars/:id");
+            }
+            CarService.prototype.getCars = function () {
+                return this.carResource.query();
+            };
+            // Method that will get a single car
+            CarService.prototype.getCar = function (id) {
+                return this.carResource.get({ id: id });
+            };
+            CarService.prototype.saveCar = function (carToSave) {
+                return this.carResource.save(carToSave).$promise;
+            };
+            CarService.prototype.deleteCar = function (id) {
+                return this.carResource.delete({ id: id }).$promise;
+            };
+            return CarService;
+        }());
+        Services.CarService = CarService;
+        angular.module("GroupProjectStart").service('carService', CarService);
+    })(Services = GroupProjectStart.Services || (GroupProjectStart.Services = {}));
+})(GroupProjectStart || (GroupProjectStart = {}));
+var GroupProjectStart;
+(function (GroupProjectStart) {
+    var Services;
+    (function (Services) {
+        console.log("Test");
+    })(Services = GroupProjectStart.Services || (GroupProjectStart.Services = {}));
 })(GroupProjectStart || (GroupProjectStart = {}));
 //# sourceMappingURL=all.js.map
