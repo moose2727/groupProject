27a4315b6b36c65f1ffb60a696fa03ed299d1dff
Ayesha <mrsayesha@outlook.com>
2016-04-18 19:59:48 -0700

    Merging changed from master +  Adding changes for editing car information

---
+++ GroupProjectStart/src/GroupProjectStart/wwwroot/ngApp/controllers/CarsEditController.ts
@@ -0,0 +1,18 @@
+﻿namespace GroupProjectStart.Controllers {
+
+
+
+    export class CarsEditController {
+
+        public carToEdit;
+
+        constructor(private carService: GroupProjectStart.Services.CarService,
+            private $stateParams: ng.ui.IStateParamsService) {
+
+            // extracts the car id from the url using $stateParams
+            let carid = this.$stateParams['id'];
+            this.carToEdit = this.carService.getCar(carid);
+
+        }
+    }
+}
